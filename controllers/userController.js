ac445764bba8934acf339cc9d8853de8b348845d
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2022-02-13 23:56:01 +0530

    origin

--- controllers/userController.js
+++ controllers/userController.js
@@ -156,3 +156,11 @@ exports.passwordReset = BigPromise(async(req,res,next)=>{
 
     cookieToken(user,res)
 });
+exports.getLoggedInUserDetails = BigPromise(async(req,res,next)=>{
+    
+    const user = await User.findById(req.user.id)
+    res.status(200).json({
+        success:true,
+        user
+    });
+});
--- middlewares/user.js
+++ middlewares/user.js
@@ -5,7 +5,7 @@ const jwt  = require("jsonwebtoken");
 
 
 exports.isLoggedIn = BigPromise(async(req,res,next)=>{
-    const token = req.cookies.token || req.header("Authorization").replace('Bearer ','')
+    const token = await req.cookies.token || req.header("Authorization").replace('Bearer ','')
     if(!token){
         return next(new CustomError('Login first to access this page',401))
     }
